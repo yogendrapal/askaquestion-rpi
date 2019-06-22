145634480d65c322bae7e4ce71644ee2c5daf602
cslite <tussank.gupta@gmail.com>
2019-06-22 15:53:30 +0530

    GPIO

--- main_rpi.py
+++ main_rpi.py
@@ -1,6 +1,15 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from config import *
+import logger
+import record
+import random
+import sync
+import os
+
+avr = record.AV_Recorder()
 
 def button1(channel):
+	global avr
 	print("Button on pin 12 was pushed!")
 	if avr.is_recording():
 		avr.stop()
@@ -11,12 +20,16 @@ def button1(channel):
 		avr.record(OUTPUT_DIR+fname)
 
 def button2(channel):
+	global avr
 	print("Button on pin 16 was pushed!")
 	avr.discard()
 
 def button3(channel):
+	global avr
 	print("Button on pin 18 was pushed!")
 	sync.sync2server()
+
+
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
@@ -24,9 +37,9 @@ GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pi
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.add_event_detect(12,GPIO.RISING,callback=button1) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(16,GPIO.RISING,callback=button2)
-GPIO.add_event_detect(18,GPIO.RISING,callback=button3)
+GPIO.add_event_detect(12,GPIO.FALLING,callback=button1) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(16,GPIO.FALLING,callback=button2)
+GPIO.add_event_detect(18,GPIO.FALLING,callback=button3)
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
