0387dfe6c4be91f2e5ab499e820523155cae8c2d
cslite <tussank.gupta@gmail.com>
2019-06-25 17:01:09 +0530

    errors resolved

--- main_rpi.py
+++ main_rpi.py
@@ -10,47 +10,44 @@ from tkinter import *
 from PIL import ImageTk,Image  
 import threading
 
-height=240
-width=320
+
 root = None
 canvas = None
 
-def first():
-	global height
-	global width
-	global root
-	global canvas
-	root = Tk()  
-	canvas = Canvas(root, width = width, height = height)  
-	canvas.pack() 
-	img = Image.open("images/first.png")  # PIL solution
-	img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
-	img = ImageTk.PhotoImage(img) 
-	#img = ImageTk.PhotoImage(Image.open("Photo.jpg"))  
-	canvas.create_image(0, 0, anchor="nw", image=img) 
-	root.mainloop()
+class MyTkApp(threading.Thread):
+    def __init__(self):
+		self.root=Tk()
+		self.height=240
+		self.width=320
+		self.canvas = Canvas(root, width = width, height = height)  
+		self.canvas.pack() 
+		img = Image.open("images/first.png")  # PIL solution
+		img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
+		img = ImageTk.PhotoImage(img) 
+		#img = ImageTk.PhotoImage(Image.open("Photo.jpg"))  
+		self.canvas.create_image(0, 0, anchor="nw", image=img)
+		threading.Thread.__init__(self)
 
-def Rec_start():
-	global height
-	global width
-	global root
-	global canvas 
-	img = Image.open("images/record3.png")  # PIL solution
-	img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
-	img = ImageTk.PhotoImage(img) 
-	#img = ImageTk.PhotoImage(Image.open("record3.png"))  
-	canvas.create_image(0, 0, anchor="nw", image=img) 
+	def Rec_start():
+		img = Image.open("images/record3.png")  # PIL solution
+		img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
+		img = ImageTk.PhotoImage(img) 
+		#img = ImageTk.PhotoImage(Image.open("record3.png"))  
+		self.canvas.create_image(0, 0, anchor="nw", image=img)
+		self.root.update()
 
-def Rec_stop():
-	global height
-	global width
-	global root
-	global canvas  
-	img = Image.open("images/record1.png")  # PIL solution
-	img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
-	img = ImageTk.PhotoImage(img) 
-	#img = ImageTk.PhotoImage(Image.open("record1.png"))  
-	canvas.create_image(0, 0, anchor="nw", image=img)  
+	def Rec_stop():
+		img = Image.open("images/record1.png")  # PIL solution
+		img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
+		img = ImageTk.PhotoImage(img) 
+		#img = ImageTk.PhotoImage(Image.open("record1.png"))  
+		self.canvas.create_image(0, 0, anchor="nw", image=img)
+		self.root.update()
+
+	def run(self):
+		self.root.mainloop()
+
+   
 
 avr = record.AV_Recorder()
 fname = ""
@@ -63,21 +60,21 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 time.sleep(1)
-
+app = MyTkApp()
+app.start()
 print('Program Started...')
-threading.Thread(target=first)
 while True: # Run forever
 	if GPIO.input(12) == 0:
 		print("Button on pin 12 was pushed!")
 		if avr.is_recording():
 			avr.stop()
-			Rec_stop()
+			app.Rec_stop()
 			logger.new_log_entry(fname,avr.ext)
 			print('Video was saved as "'+fname + '.' + avr.ext +'"\n')
 		else:
 			fname = 'vid' + str(random.randint(100,1001))
 			avr.record(OUTPUT_DIR+fname)
-			Rec_start()
+			app.Rec_start()
 		time.sleep(3)
 
 	if GPIO.input(16) == 0:
