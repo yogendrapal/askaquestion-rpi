9987ae4ec9a4e6f720adf6f1e0ae20eeabca2a93
cslite <tussank.gupta@gmail.com>
2019-06-25 16:47:33 +0530

    main_rpi update

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HOST = '10.196.17.146'
 API_PORT = 8081
 OUTPUT_DIR = 'output/'
 RECORD_VIDEO_ONLY = False
-RPI_CMD = False
+RPI_CMD = True
 LOW_SETTING = False
 
 DB_PATH = 'database.sqlite3'
--- main_rpi.py
+++ main_rpi.py
@@ -1,61 +1,3 @@
-# import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-# from config import *
-# import logger
-# import record
-# import random
-# import sync
-# import os
-# import time
-
-# avr = record.AV_Recorder()
-# fname = ""
-
-# def button1(channel):
-# 	global avr
-# 	global fname
-# 	print("Button on pin 12 was pushed!")
-# 	# if avr.is_recording():
-# 	# 	avr.stop()
-# 	# 	logger.new_log_entry(fname,avr.ext)
-# 	# 	print('Video was saved as "'+fname + '.' + avr.ext +'"\n')
-# 	# else:
-# 	fname = 'vid' + str(random.randint(100,1001))
-# 	avr.record(OUTPUT_DIR+fname)
-# 	return
-
-
-# def button2(channel):
-# 	global avr
-# 	print("Button on pin 16 was pushed!")
-# 	if avr.is_recording():
-# 		avr.stop()
-# 		logger.new_log_entry(fname,avr.ext)
-# 		print('Video was saved as "'+fname + '.' + avr.ext +'"\n')
-# 	# avr.discard()
-
-# def button3(channel):
-# 	global avr
-# 	print("Button on pin 18 was pushed!")
-# 	sync.sync2server()
-
-
-
-# GPIO.setwarnings(False) # Ignore warning for now
-# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-# GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-# GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# GPIO.add_event_detect(12,GPIO.FALLING,callback=button1) # Setup event on pin 10 rising edge
-# GPIO.add_event_detect(16,GPIO.FALLING,callback=button2)
-# GPIO.add_event_detect(18,GPIO.FALLING,callback=button3)
-
-# message = input("Press enter to quit\n\n") # Run until someone presses enter
-
-# GPIO.cleanup() # Clean up
-
-
-
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from config import *
 import logger
@@ -64,13 +6,57 @@ import random
 import sync
 import os
 import time
-import tt
-import tt4
+from tkinter import *  
+from PIL import ImageTk,Image  
+
+height=240
+width=320
+root = None
+canvas = None
+
+def first():
+	global height
+	global width
+	global root
+	global canvas
+	root = Tk()  
+	canvas = Canvas(root, width = width, height = height)  
+	canvas.pack() 
+	img = Image.open("images/first.png")  # PIL solution
+	img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img = ImageTk.PhotoImage(img) 
+	#img = ImageTk.PhotoImage(Image.open("Photo.jpg"))  
+	canvas.create_image(0, 0, anchor="nw", image=img) 
+	root.mainloop()
+
+def Rec_start():
+	global height
+	global width
+	global root
+	global canvas 
+	img = Image.open("images/record3.png")  # PIL solution
+	img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img = ImageTk.PhotoImage(img) 
+	#img = ImageTk.PhotoImage(Image.open("record3.png"))  
+	canvas.create_image(0, 0, anchor="nw", image=img) 
+	root.mainloop() 
+
+def Rec_stop():
+	global height
+	global width
+	global root
+	global canvas  
+	img = Image.open("images/record1.png")  # PIL solution
+	img = img.resize((width, height), Image.ANTIALIAS) #The (250, 250) is (height, width)
+	img = ImageTk.PhotoImage(img) 
+	#img = ImageTk.PhotoImage(Image.open("record1.png"))  
+	canvas.create_image(0, 0, anchor="nw", image=img) 
+	root.mainloop()  
 
 avr = record.AV_Recorder()
 fname = ""
 
-tt.first()
+
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
@@ -79,32 +65,30 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 time.sleep(1)
 
+print('Program Started...')
+first()
 while True: # Run forever
 	if GPIO.input(12) == 0:
 		print("Button on pin 12 was pushed!")
 		if avr.is_recording():
 			avr.stop()
+			Rec_stop()
 			logger.new_log_entry(fname,avr.ext)
-			tt4.stop()
-			#print('Video was saved as "'+fname + '.' + avr.ext +'"\n')
-
+			print('Video was saved as "'+fname + '.' + avr.ext +'"\n')
 		else:
-			
 			fname = 'vid' + str(random.randint(100,1001))
 			avr.record(OUTPUT_DIR+fname)
-			tt4.stop()
-		#time.sleep(3)
+			Rec_start()
+		time.sleep(3)
 
 	if GPIO.input(16) == 0:
 		print("Button on pin 16 was pushed!")
 		avr.discard()
-		tt4.stop()
-		#time.sleep(3)
+		time.sleep(3)
 
 	if GPIO.input(18) == 0:
 		print("Button on pin 18 was pushed!")
 		sync.sync2server()
-		tt4.stop()
-		#time.sleep(3)
+		time.sleep(3)
 
 GPIO.cleanup()
